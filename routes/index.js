9927c458333fa1a7456b1c50f104d4a7d51054c4
Bowen GAO <gaobw@hes86.com>
2015-09-13 15:52:59 +0800

    en

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET en page. */
+router.get('/en', function(req, res, next) {
+  res.render('index_en', { title: 'Express' });
+});
+
 module.exports = router;
